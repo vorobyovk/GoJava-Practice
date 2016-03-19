2e76e4d8832e5618769e412c90ed11974a4fac88
Vorobyov Kirill <vorobyovk@gmail.com>
2016-03-19 17:59:03 +0200

    editing of chapter 3 iles

--- chapter3/src/Function.java
+++ chapter3/src/Function.java
@@ -1,16 +1,41 @@
 import java.util.Arrays;
-import java.util.Collections;
 
 public class Function {
 
  //3.1. Еда для гномов
     public int[] findDwarf(int[] gnames, int[] portions) {
         int[] distribution = new int[gnames.length];
-        Integer[] tempName = ;
-        Arrays.sort(tempName, Collections.reverseOrder());
 
-        distribution = tempName;
+        int[] tempName = new int[gnames.length];
+        for(int i=0;i<gnames.length;i++) tempName[i]=gnames[i];
+        Arrays.sort(tempName);
+        int[] indexName = new int[gnames.length];
+        for(int i=0;i<gnames.length;i++){
+            for(int j = 0;j<tempName.length;j++) if(gnames[i]==tempName[j]) indexName[i] = gnames.length-(j+1);
+        };
+
+        int[] tempPortion = new int[portions.length];
+        for(int i=0;i<portions.length;i++) tempPortion[i]=portions[i];
+        Arrays.sort(tempPortion);
+        int[] indexPortion = new int[portions.length];
+        for(int i=0;i<portions.length;i++){
+            for(int j = 0;j<tempPortion.length;j++) if(portions[i]==tempPortion[j]) indexPortion[i] = portions.length-(j+1);
+        };
+
+        for(int i=0;i<gnames.length;i++)
+            for(int j =0;j<portions.length;j++) if(indexName[i]==indexPortion[j]) distribution[i] = j;
+
         return distribution;
     }
+
+//3.2. Unix Путь
+    public String simplify(String input) {
+
+        return null;
+    }
+
+//3.3. Самый долгий период стабильности
+
+
 
 }
--- chapter3/src/main.java
+++ chapter3/src/main.java
@@ -5,10 +5,15 @@ public class main {
 
 
 //3.1. Еда для гномов
-     int[] dwarf = {5,7,6,9,4};
-     int[] portion = {5,7,6,9,4};
+     int[] dwarf = {4, 1, 3, 2}; //{5,7,6,9,4};
+     int[] portion = {2, 1, 3, 4};//{8,5,6,2,3};
      int[] distribution = function.findDwarf(dwarf,portion);
      for(int i=0;i<distribution.length;i++) System.out.print(" "+distribution[i]+",");
+
+//3.2. Unix Путь
+
+
+//3.3. Самый долгий период стабильности
 
 
     }
